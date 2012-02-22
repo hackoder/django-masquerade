a0000aa1736fb1aca7620422fbd5c4ba436ccf30
Hasan Ammar <ammarh@gmail.com>
2012-02-21 21:35:38 -0500

    Minor tweaks. Start + Stop masquerading views (insetad of urls).

--- masquerade/urls.py
+++ masquerade/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^mask/$', 'masquerade.views.mask'),
-    url(r'^unmask/$', 'masquerade.views.unmask'),
+    url(r'^mask/$', 'masquerade.views.mask', name='start_masquerading'),
+    url(r'^unmask/$', 'masquerade.views.unmask', name='stop_masquerading'),
 )
--- masquerade/views.py
+++ masquerade/views.py
@@ -1,14 +1,25 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponseForbidden, Http404
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from masquerade.forms import MaskForm
 
-MASQUERADE_REDIRECT_URL = getattr(settings, 'MASQUERADE_REDIRECT_URL', '/')
+START_MASQUERADE_REDIRECT_VIEW = getattr(settings, 'START_MASQUERADE_REDIRECT_VIEW', None)
+STOP_MASQUERADE_REDIRECT_VIEW = getattr(settings, 'STOP_MASQUERADE_REDIRECT_VIEW', None)
 
 MASQUERADE_REQUIRE_SUPERUSER = getattr(settings,
   'MASQUERADE_REQUIRE_SUPERUSER', False)
+
+def get_start_redirect_url():
+    if START_MASQUERADE_REDIRECT_VIEW:
+        return reverse(START_MASQUERADE_REDIRECT_VIEW)
+    else: return '/'
+
+def get_stop_redirect_url():
+    if STOP_MASQUERADE_REDIRECT_VIEW:
+        return reverse(STOP_MASQUERADE_REDIRECT_VIEW)
+    else: return '/'
 
 def mask(request, template_name='masquerade/mask_form.html'):
     if not request.user.is_masked and not request.user.is_staff:
@@ -21,7 +32,7 @@ def mask(request, template_name='masquerade/mask_form.html'):
         if form.is_valid():
             # turn on masquerading
             request.session['mask_user'] = form.cleaned_data['mask_user']
-            return HttpResponseRedirect(MASQUERADE_REDIRECT_URL)
+            return HttpResponseRedirect(get_start_redirect_url())
     else:
         form = MaskForm()
 
@@ -35,4 +46,4 @@ def unmask(request):
     except KeyError:
         pass
 
-    return HttpResponseRedirect(MASQUERADE_REDIRECT_URL)
+    return HttpResponseRedirect(get_stop_redirect_url())
